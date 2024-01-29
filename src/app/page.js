6437a4ab72c41226984fb5da0dd70a05660f9865
Huzaifa-A750 <huzaifaafzal750@gmail.com>
2024-01-30 02:15:30 +0500

    UI updates

--- src/app/page.js
+++ src/app/page.js
@@ -4,55 +4,53 @@ const questions = [
   {
     id: 1,
     text: "IF YOU HAD TO CREATE A OPTION , WHAT WOULD YOU MAKE?",
-    backgroundanimated: true, 
+    backgroundanimated: true,
     options: [
       {
         label: "ETERNITY",
         color: "Pink",
         image: "/Assets/choose-a-option/primary.svg",
-        backgroundanimated: false, 
+        backgroundanimated: false,
       },
       {
         label: "INTELLECT",
         color: "Blue",
         image: "/Assets/choose-a-option/secondary.svg",
-        backgroundanimated: false, 
+        backgroundanimated: false,
       },
       {
         label: "CREATIVITY",
         color: "Yellow",
         image: "/Assets/choose-a-option/tertiary.svg",
-        backgroundanimated: false, 
+        backgroundanimated: false,
       },
     ],
   },
   {
     id: 2,
-    text: "IF YOU HAD ANY POWER WHAT WOULD YOU CHOOSE?",    options: [
+    text: "IF YOU HAD ANY POWER WHAT WOULD YOU CHOOSE?",
+    options: [
       {
         label: "STRENGTH",
         color: "Yellow",
         image: "/Assets/choose-a-power/primary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "SHAPESHIFT",
         color: "Blue",
         image: "/Assets/choose-a-power/secondary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "HEAL",
         color: "Blue",
         image: "/Assets/choose-a-power/tertiary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
     ],
   },
-  
+
   {
     id: 3,
     text: "WHAT ARE  YOU MOST LOOKING FORWARD TO LEARNING AT SCHOOL OF INVENTION ?",
@@ -61,22 +59,19 @@ const questions = [
         label: "FLYING",
         color: "Yellow",
         image: "/Assets/school-of-invention/primary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "TELEPORTING",
         color: "Blue",
         image: "/Assets/school-of-invention/secondary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "CURING",
         color: "Pink",
         image: "/Assets/school-of-invention/tertiary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
     ],
   },
@@ -88,22 +83,19 @@ const questions = [
         label: "TOOLS",
         color: "Blue",
         image: "/Assets/three-boxes/primary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "PAINTS",
         color: "Yellow",
         image: "/Assets/three-boxes/secondary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "BLOOD",
         color: "Pink",
         image: "/Assets/three-boxes/tertiary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
     ],
   },
@@ -115,22 +107,19 @@ const questions = [
         label: "RIVER",
         color: "Pink",
         image: "/Assets/choose-to-continue/primary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "TREE",
         color: "Yellow",
         image: "/Assets/choose-to-continue/secondary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "MOON",
         color: "Blue",
         image: "/Assets/choose-to-continue/tertiary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
     ],
   },
@@ -138,15 +127,9 @@ const questions = [
     id: 6,
     text: "",
     options: [
-      { label: "DANCE", color: "Yellow", image: "" , 
-      backgroundanimated: true, 
-    },
-      { label: "DISCOVER", color: "Blue", image: "" ,
-      backgroundanimated:true, 
-    },
-      { label: "DREAM", color: "Pink", image: "",
-      backgroundanimated: true, 
-    },
+      { label: "DANCE", color: "Yellow", image: "", backgroundanimated: true },
+      { label: "DISCOVER", color: "Blue", image: "", backgroundanimated: true },
+      { label: "DREAM", color: "Pink", image: "", backgroundanimated: true },
     ],
   },
   {
@@ -157,22 +140,19 @@ const questions = [
         label: "",
         color: "Blue",
         image: "/Assets/choose-a-pet/primary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "",
         color: "Pink",
         image: "/Assets/choose-a-pet/secondary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
       {
         label: "",
         color: "Yellow",
         image: "/Assets/choose-a-pet/tertiary.svg",
-        backgroundanimated: false, 
-
+        backgroundanimated: false,
       },
     ],
   },
@@ -183,8 +163,8 @@ export default function Home() {
   const [answers, setAnswers] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [sortingCeremonyEnded, setSortingCeremonyEnded] = useState(false);
-  const [status , setStatus] = useState(false)
- const [result , setResults] = useState("")
+  const [status, setStatus] = useState(false);
+  const [result, setResults] = useState("");
   const startQuiz = () => {
     setQuizStarted(true);
   };
@@ -205,7 +185,7 @@ export default function Home() {
     } else {
       const result = calculateResult();
       setSortingCeremonyEnded(true);
-      setResults(result)
+      setResults(result);
     }
   };
 
@@ -232,20 +212,20 @@ export default function Home() {
     return mostSelected;
   };
   const handleContinue = () => {
-    setStatus(true)
+    setStatus(true);
   };
 
   return (
     <div
       className={
         quizStarted
-          ? "bg-container relative text-white bg-black"
-          : "h-screen pb-12 bg-black z-[10] text-white"
+          ? "bg-container relative text-white bg-neutral-800 grid items-center"
+          : "pb-12 bg-neutral-800 grid items-center h-[100vh] z-[10] text-white"
       }
     >
-      <div className="mx-auto pt-36 max-w-[1000px]">
+      <div className="mx-auto max-w-[1000px]">
         {!quizStarted ? (
-          <div className="flex flex-col mx-auto pt-10">
+          <div className="flex flex-col mx-auto">
             <img src="/Assets/logo.svg" className="h-28 w-28 mx-auto" alt="" />
             <h1 className="mx-auto lg:text-[48px] pt-[24px] font-bold">
               DISCOVER YOUR HOUSE
@@ -253,7 +233,7 @@ export default function Home() {
             <div className="mx-auto pt-10">
               <button
                 onClick={startQuiz}
-                className="text-center hover:text-[#00FFFF] border-[#00FFFF] border-2 py-4 rounded-md px-12  text-3xl font-bold"
+                className="text-center font-[ConthraxSb-Regular] hover:text-[#00FFFF] border-[#00FFFF] border-2 py-4 rounded-md px-12  text-3xl font-bold"
               >
                 START THE SORTING CEREMONY
               </button>
@@ -265,12 +245,12 @@ export default function Home() {
               <div className="z-[1000]">
                 <ul>
                   <br />
-                  <li className="flex lg:text-[28px] font-bold justify-center text-center">
+                  <li className="flex lg:text-[28px] pb-3 font-bold justify-center text-center">
                     {questions[currentQuestion].text} <br />
                   </li>
                   <br />
                 </ul>
-                <div className="lg:flex cursor-pointer lg:justify-between grid justify-center ">
+                <div className="lg:flex cursor-pointer gap-20 lg:justify-between grid justify-center ">
                   {questions[currentQuestion].options.map((option) => (
                     <div
                       key={option.label}
@@ -286,11 +266,13 @@ export default function Home() {
                           />
                         </div>
                       )}
-                     <button
-  className={`lg:text-[30px] mt-8  font-bold ${
-    option.backgroundanimated ? 'bg-gray-500 rounded-md px-8 py-3' : ''
-  }`}
->
+                      <button
+                        className={`lg:text-[25px] mt-7 bg-opacity-50 font-bold ${
+                          option.backgroundanimated
+                            ? "bg-gray-500 rounded-md px-12 py-0.5 lg:text-[44px]"
+                            : ""
+                        }`}
+                      >
                         {option?.label}
                         <br />
                       </button>
@@ -304,75 +286,101 @@ export default function Home() {
       </div>
       {sortingCeremonyEnded && (
         <>
-        {!status && 
-        <div className="absolute top-0 w-[100%] left-0 bg-container h-[100%]">
-          <div className="grid justify-center items-center pt-[400px]">
-            <h1 className="text-[32px] font-bold max-w-[600px] text-center">
-              THE SORTING CEREMONY IS ABOUT TO END
-            </h1>
-            <button
-              onClick={handleContinue}
-              className="text-center font-bold my-3 border-[#00FFFF] border-2 py-4 rounded-md px-12"
-            >
-              CONTINUE
-            </button>
-        
-          </div>
-        </div>
-        }
-       </>
+          {!status && (
+            <div className="absolute top-0 items-center grid left-0 bg-container h-[100%]">
+              <div className="grid justify-center ">
+                <h1 className="text-[32px] font-bold max-w-[600px] text-center">
+                  THE SORTING CEREMONY IS ABOUT TO END
+                </h1>
+                <div className="grid pt-14 justify-center">
+                  <button
+                    onClick={handleContinue}
+                    className="text-center font-bold my-3 border-[#00FFFF] w-[340px] border-2 py-4 rounded-md px-12"
+                  >
+                    CONTINUE
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </>
       )}
-{status && (
-  <div className="flex flex-col justify-center -mt-20 items-center h-screen">
-    {result === "Yellow" && (
-      <>
-      <p className="lg:text-[58px] font-bold">CONGRATULATIONS!</p>
-        <p className="lg:text-[43px] font-semibold">YOU ARE SELECTED FOR...</p>
-      <div>
-      <img className="size-48 animate-ping" src="/Assets/yellow-status.svg" alt="yellow"    />
-      </div>
-      <div className="max-w-[600px] px-12 py-5 rounded-md bg-yellow-500 text-4xl font-bold">
-     HOUSE OF GADGETS
-      </div>
-      <button className="pt-40" onClick={() => window.location.reload()}>
-BACK TO THE MAIN MENU
-</button>
-      </>
-    )}
-    {result === "Blue" && (
-      <>
-            <p className="lg:text-[58px] font-bold">CONGRATULATIONS!</p>
-        <p className="lg:text-[43px] font-semibold">YOU ARE SELECTED FOR...</p>
-         <div>
-      <img className="size-48 animate-ping" src="/Assets/blue-status.svg" alt="yellow"    />
-      </div>
-        <div className="max-w-[600px] px-12 py-5 rounded-md bg-blue-500  text-4xl font-bold">
-     HOUSE OF PACE
-      </div>
-      <button className="pt-40" onClick={() => window.location.reload()}>
-BACK TO THE MAIN MENU
-</button>
-
-      </>
-    )}
-    {result === "Pink" && (
-      <>
-           <p className="lg:text-[58px] font-bold">CONGRATULATIONS!</p>
-        <p className="lg:text-[43px] font-semibold">YOU ARE SELECTED FOR...</p>
-         <div>
-      <img className="size-48 animate-ping" src="/Assets/pink-status.svg" alt="yellow"    />
-      </div>
-      <div className="max-w-[600px] px-12 py-5 rounded-md bg-pink-500  text-4xl font-bold">
-     HOUSE OF LIFE
-      </div>
-      <button className="pt-40" onClick={() => window.location.reload()}>
-BACK TO THE MAIN MENU
-</button>
-      </>
-    )}
-  </div>
-)}
-
+      {status && (
+        <div className="flex flex-col justify-center items-center">
+          {result === "Yellow" && (
+            <>
+              <p className="lg:text-[58px] font-bold">CONGRATULATIONS!</p>
+              <p className="lg:text-[43px] font-semibold">
+                YOU ARE SELECTED FOR...
+              </p>
+              <div>
+                <img
+                  className="mt-6"
+                  src="/Assets/yellow-status.svg"
+                  alt="yellow"
+                />
+              </div>
+              <div className="max-w-[600px] px-12 py-5 pt-12 rounded-md bg-yellow-500 text-4xl font-bold">
+                HOUSE OF GADGETS
+              </div>
+              <button
+                className="pt-40"
+                onClick={() => window.location.reload()}
+              >
+                BACK TO THE MAIN MENU
+              </button>
+            </>
+          )}
+          {result === "Blue" && (
+            <>
+              <p className="lg:text-[45px] font-bold">CONGRATULATIONS!</p>
+              <p className="lg:text-[45px] tracking-[0.1rem] font-semibold">
+                YOU ARE SELECTED FOR...
+              </p>
+              <div>
+                <img
+                  className="mt-6"
+                  src="/Assets/blue-status.svg"
+                  alt="yellow"
+                />
+              </div>
+              <div className="max-w-[600px] px-12 py-5 rounded-md bg-blue-500 mt-12 text-4xl font-bold">
+                HOUSE OF PACE
+              </div>
+              <button
+                className="pt-40 pb-2"
+                onClick={() => window.location.reload()}
+              >
+                BACK TO THE MAIN MENU
+              </button>
+            </>
+          )}
+          {result === "Pink" && (
+            <>
+              <p className="lg:text-[35px] font-bold">CONGRATULATIONS...</p>
+              <p className="lg:text-[35px] font-semibold">
+                YOU ARE SELECTED FOR
+              </p>
+              <div>
+                <img
+                  className="mt-6"
+                  src="/Assets/pink-status.svg"
+                  alt="yellow"
+                />
+              </div>
+              <div className="max-w-[600px] px-12 py-5 rounded-md bg-pink-500  text-4xl font-bold">
+                HOUSE OF LIFE
+              </div>
+              <button
+                className="pt-40 pb-2"
+                onClick={() => window.location.reload()}
+              >
+                BACK TO THE MAIN MENU
+              </button>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
